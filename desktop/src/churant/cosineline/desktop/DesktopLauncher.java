ed47a363aab541728668954efb4fd3cb6face92f
Ethan Plant <plant.ethan@gmail.com>
2017-12-18 12:13:58 -0500

    Initial Commit

---
+++ desktop/src/churant/cosineline/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package churant.cosineline.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import churant.cosineline.GamCosineLine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GamCosineLine(), config);
+	}
+}
