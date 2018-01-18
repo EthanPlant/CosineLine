5fd923e5a0d828779ffa7826959620e4049855ec
churj8630 <churj8630@wrdsb.ca>
2018-01-18 13:37:55 -0500

    rotation actually works this time

--- core/src/churant/cosineline/sprites/Player.java
+++ core/src/churant/cosineline/sprites/Player.java
@@ -4,9 +4,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Player extends Sprite {
-    
-    private float fDx, fDy;
-    
+
+    private float fDx, fDy, fRotation;
+    private boolean isRotated;
+
     public Player(float fX, float fY) {
         setBounds(0, 0, 200, 200);
         setRegion(new Texture("SprMain.png"));
@@ -14,18 +15,35 @@ public class Player extends Sprite {
         setPosition(fX, fY);
         fDx = 15;
         fDy = 0;
+        fRotation = 0;
+        isRotated = false;
     }
-    
+
     public void update() {
         if (this.getX() <= 120) {
             fDx += 1;
+            if (isRotated == false) {
+                fRotation -= 1;
+                isRotated = !isRotated;
+            } else if (isRotated == true) {
+                fRotation -= 2;
+            }
         }
         if (this.getX() >= 855) {
             fDx -= 1;
+            if (isRotated == false) {
+                fRotation += 1;
+                isRotated = true;
+                isRotated = !isRotated;
+            } else if (isRotated == true) {
+                fRotation += 2;
+            }
         }
+        
         setPosition(this.getX() + fDx, this.getY() + fDy);
+        setRotation(fRotation);
     }
-    
+
     public void setDeltaY(float fValue) {
         fDy = fValue;
     }
